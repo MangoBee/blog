057026e4cff5f3ae25b2c0ba65eda89fbeabd5fa
MangoBee <twin.faam@gmail.com>
2024-05-09 23:19:17 +0200

    Quartz sync: May 9, 2024, 11:19 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -32,6 +32,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
+    Component.RecentNotes(),
   ],
 }
 
