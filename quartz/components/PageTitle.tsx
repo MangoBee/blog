6ee0680878da1dfdf5300acb8eedeacaa3fbd3c6
MangoBee <twin.faam@gmail.com>
2024-05-11 17:07:17 +0200

    Quartz sync: May 11, 2024, 5:07 PM

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -7,7 +7,7 @@ import { i18n } from "../i18n"
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
-  const iconPath = joinSegments(baseDir, "static/Logo.png")
+  const iconPath = joinSegments(baseDir, "static/icon.png")
   return (
     <h1 class={classNames(displayClass, "page-title")}>
       <a href={baseDir}>
